a9f27d9952ffe029cbecf5774360ca1e10f9ba2a
“rohan335” <rohandesai@Rohans-MacBook-Pro.local>
2021-03-27 19:16:12 -0400

    finished

---
+++ src/logging/logger.ts
@@ -0,0 +1,122 @@
+//all my imports, this file is only for the logger, so it uses winston and daily rotate
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+//create a new dailyrotatefile transport for evvery level
+//done like this because transports do not autosort by levels when using dailyrotate for some reason 
+var error = new winston.transports.DailyRotateFile({
+    level: "error",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/error-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var warn = new winston.transports.DailyRotateFile({
+    level: "warn",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/warn-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var info = new winston.transports.DailyRotateFile({
+    level: "info",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/info-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var http = new winston.transports.DailyRotateFile({
+    level: "http",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/http-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var verbose = new winston.transports.DailyRotateFile({
+    level: "verbose",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/verbose-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var debug = new winston.transports.DailyRotateFile({
+    level: "debug",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/debug-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+var silly = new winston.transports.DailyRotateFile({
+    level: "silly",
+    format: winston.format.combine(winston.format.timestamp(),winston.format.prettyPrint()),
+    filename: 'logs/silly-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '50m',
+})
+
+//upon every transport rotating a file we console.log that the rotatition was successful
+error.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+  });
+warn.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+info.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+http.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+verbose.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+debug.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+silly.on('rotate', function(oldFilename: any, newFilename: any) {
+    console.log("rotated", oldFilename, "to", newFilename);
+});
+
+//creates a logger for every level
+export const errorLogger = winston.createLogger({
+    transports:[
+        error
+    ]
+});
+export const warnLogger = winston.createLogger({
+    transports:[
+        warn
+    ]
+});
+export const infoLogger = winston.createLogger({
+    transports:[
+        info
+    ]
+});
+export const httpLogger = winston.createLogger({
+    transports:[
+        http
+    ]
+});
+export const verboseLogger = winston.createLogger({
+    transports:[
+        verbose
+    ]
+});
+export const debugLogger = winston.createLogger({
+    transports:[
+        debug
+    ]
+});
+export const sillyLogger = winston.createLogger({
+    transports:[
+        silly
+    ]
+});
--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,21 @@
  * @file server.ts
  */
 
+//imports
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-
 import { decodeFirebaseIdToken } from "./auth/auth";
 import firebaseInit from "./auth/firebaseInit";
-import connectMongoose from "./db/connectMongoose";
 import demoLogger from "./logging/demoLogger";
+import bodyParser from "body-parser"
+import * as loggers from "./logging/logger";
 
 // Make sure .env with Firebase project info is in the root of the directory or this will fail.
 dotenv.config();
 
 // Only call these once on server start.
 firebaseInit();
-connectMongoose();
 
 const port = process.env["PORT"] || 3000;
 
@@ -29,9 +29,35 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(decodeFirebaseIdToken);
+//Middleware specific to logging server
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 // When testing, let supertest find the first available port so no port collisions occur between jest workers.
 if (process.env["NODE_ENV"] !== "test") {
+
+  app.post('/post-test', (req, res) => {
+    //I hate Ts variables using references, so this is the only way I could get it to make a copy of the JSON object that was not a reference 😐
+    //Fuck you Ts
+    var temp = JSON.parse(JSON.stringify(req.body));
+    //remove the type in the copy, since keeping it in would be redundant. its already the log in as "level"
+    delete temp['type'];
+    
+    //if statements to sort the request level to the correct logger
+    //logs with the ccorrect logger
+    if(req.body.type == "error") {loggers.errorLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "warn") {loggers.warnLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "info") {loggers.infoLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "http") {loggers.httpLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "verbose") {loggers.verboseLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "debug") {loggers.debugLogger.log(req.body.type, temp, 200)}
+    if(req.body.type == "silly") {loggers.sillyLogger.log(req.body.type, temp, 200)}
+    
+    //returns HTTP OK 200 🚀🚀🚀
+    res.sendStatus(200);
+  })
+
   app.listen(port, () => console.log(`Server listening on port ${port}...`));
 }
 
